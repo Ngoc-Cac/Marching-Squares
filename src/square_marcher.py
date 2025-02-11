d6ff8128d2e993e36f29f28bb8ff2ac253c14793
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-02-11 19:05:40 +0700

    update docstring

--- src/square_marcher.py
+++ src/square_marcher.py
@@ -1,15 +1,15 @@
 import random, sys
 
-
 import numpy as np
-
-from matplotlib.axes import Axes
-from matplotlib.image import AxesImage
-from matplotlib.lines import Line2D
 
 from pynoise.noisemodule import Perlin
 
 
+from matplotlib.axes import Axes
+from matplotlib.colors import Colormap
+from matplotlib.image import AxesImage
+from matplotlib.lines import Line2D
+from matplotlib.typing import ColorType
 from typing import (
     Optional,
     TypeAlias,
@@ -76,8 +76,13 @@ def _draw_contour(grid: np.ndarray[NumericType],
         plt.plot(*edges, color='blue')
 
     ## Parameters:
-    ``grid``: a `numpy.ndarray` of shape `(rows, cols)`. Each element should be a scalar\\
-    ``threshold``: a number representing the threshold to seperate the scalars. The scalars\
+    ``grid: numpy.ndarray[int | float]``
+        
+        a `numpy.ndarray` of shape `(rows, cols)`. Each element should be a scalar
+
+    ``threshold: int | float``
+    
+        a number representing the threshold to seperate the scalars. The scalars\
         will be divided into two groups using the threshold: one being greater than the threshold\
         and one being less than or equal to the threshold.
 
@@ -185,8 +190,8 @@ class SquareMarcher():
         ax: Optional[Axes],
         z: Optional[NumericType] = None,
         speed: Optional[NumericType] = None, *,
-        line_color: tuple[float, float, float] = (1, 1, .5608),
-        cmap: str = 'Greys',
+        line_color: ColorType = (1, 1, .5608),
+        cmap: str | Colormap = 'Greys',
         dot_marker: str = 'o',
         animated: bool = False
     ) -> tuple[Axes, Optional[list[AxesImage, Line2D]]]:
@@ -194,31 +199,46 @@ class SquareMarcher():
         Run Marching Squares on a Perlin noisemap generated with the current configuration.
 
         ## Parameters:
-        ``ax``: a ``matplotlib.axes.Axes`` object to plot on.
-
-        ``z``: the z-level to generate Perlin noise. If None is given, a uniform random\
+        ``ax: matplotlib.axes.Axes | None``
+            
+            a ``matplotlib.axes.Axes`` object to plot on.
+        
+        ``z: int | float``
+        
+            the z-level to generate Perlin noise. If None is given, a uniform random\
             number between 0-1 and chosen instead.
 
-        ``speed``: a number specifying how much to increment x and y when moving along\
+        ``speed: int | float``
+
+            a number specifying how much to increment x and y when moving along\
             the plane levelled at z. A big increment will lead to a noisier noisemap.
 
-        ``line_color``: the color of contour lines. This defaults to ``(1, 1, 0.5608)``
+        ``line_color: ColorType``
+        
+            the color of contour lines, should be a matplotlib's [color format](https://matplotlib.org/stable/users/explain/colors/colors.html#colors-def).\
+            This defaults to ``(1, 1, 0.5608)``
 
-        ``cmap``: the colormap to render the noisemap. This defaults to ``'Greys'``.
-
-            *Note: this only takes effect for grids of dimension higher than 20.*\\
+        ``cmap: str | Colormap``
+        
+            the colormap to render the noisemap, should be a matplotlib's colormap.\
+                This defaults to ``'Greys'``.\\
+            *Note: this only takes effect for grids of dimension higher than 20.*
             *For dimension 20 and below, noisemap is rendered as grid of points.*
 
-        ``dot_marker``: matplotlib's point marker used for rendering the noisemap.
-
+        ``dot_marker: str``
+        
+            matplotlib's point marker used for rendering the noisemap.\\
             *Note: this only takes effect for grids of dimension 20 and below. See above.*
 
-        ``animated``: whether or not to plot the artists with animated artists.\\
-        Animated objects are not drawn to canvas unless artist.set_visible(True) is\
-            called. This is useful when you need to create animation with\
+        ``animated: bool``
+            
+            whether or not to plot the artists with animated artists.\
+                This defualts to ``False``.\\
+            Animated objects are not drawn to canvas unless artist.set_visible(True) is\
+                called. This is useful when you need to create animation with\
             ``matplotlib.animation.ArtistAnimation`` or similar means.\\
-        If ``animated=True``, the function will return back a list of ``matplotlib.artist.Artists``\
-            to be drawn on the given ``matplotlib.axes.Axes``.
+            If ``animated=True``, the function will return back a list of\
+                ``matplotlib.artist.Artists`` to be drawn on the given ``matplotlib.axes.Axes``.
 
         ## Returns
         A tuple of ``matplotlib.axes.Axes`` and optional list of ``matplotlib.artist.Artists``\
