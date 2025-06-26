6daf33f2afe4b1c647807e7dfd04a73d2d7a364a
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-06-26 20:36:36 +0700

    update docstrings to follow sphinx format

--- src/marching_squares/algo.py
+++ src/marching_squares/algo.py
@@ -13,29 +13,19 @@ def edge_lerp(
 ) -> float:
     """
     Linear interpolation between two corners' values in Marching Square according to
-        a threshold ``t``.
-    Think of this as drawing a line between ``(v_1, 0)`` and ``(v_2, 1)`` and then
-        evaluate that line at the value ``t``. This gives the offest on the edge.
+    a threshold `t`.
+    Think of this as drawing a line between `(v_1, 0)` and `(v_2, 1)` and then
+    evaluate that line at the value `t`. This gives the offest on the edge.
 
-    ## Parameters:
-    ``t: NumericType``
+    
+    :param int or float t: The parameter at which to evaluate the interpolation.
+    :param int or float v_1: The value of first corner. This should be the corner
+        where there is no offset from the the top left corner.
+    :param int or float v_1: The value of the second corner. This is the corner
+        with the offset.
 
-        the parameter at which to evaluate the interpolation.
-
-    ``v_1: NumericType``
-
-        the value of first corner. This should be the corner where there is no
-            offset from the the top left corner.
-
-    ``v_2: NumericType``
-
-        the value of the second corner. This is the corner with the offset.
-
-    ## Return
-    A value representing the offset on the edge.
-
-    ## Raises
-    ZeroDivisionError if ``v_1 == v_2``
+    :return float: A value representing the offset on the edge.
+    :raise ZeroDivisionError: if `v_1 == v_2`
     """
     return (t - v_1) / (v_2 - v_1)
 
@@ -76,8 +66,8 @@ def _smooth_contouring(
     threshold: NumericType
 ) -> tuple[tuple[float, float, float, float]]:
     """
-    Drawing contour lines based on the four corners' states with linear interpolation\
-        using their values to find the intersection.
+    Drawing contour lines based on the four corners' states with linear interpolation
+    using their values to find the intersection.
     """
     case_no = tleft * 8 + tright * 4 + bright * 2 + bleft
     x, y = coordinates
@@ -177,48 +167,42 @@ def draw_contours(
     lerp: bool = False
 ) -> list[tuple[float, float]]:
     """
-    The Marching Squares algorithm in its entirety.\\
+    The Marching Squares algorithm in its entirety.
     This is supposed to be a multi-purpose function for internal use.
+    The function will return a list of edges. Each element of the list is
+    a tuple of two coordinates.
 
-    The function will return a list of edges. Each element of the list is\
-        a tuple of two coordinates.\\
-    **This list should be read in increments of 2**.\
-        The first tuple is the `(x_from, x_to)` coordinates of the edge.\
-        The second tuple is the `(y_from, y_to)` coordinates of the edge.
+    **This list should be read in increments of 2**.
+    The first tuple is the `(x_from, x_to)` coordinates of the edge.
+    The second tuple is the `(y_from, y_to)` coordinates of the edge.
 
-    This is configured for convenience of matplotlib plotting. If you have no intention\
-        of post-processing the edges, and just want to visualise the edges using `ax.plot`\
-        or similar means, see the below example::
-        
-        import matplotlib.pyplot as plt
-        import numpy as np
-        from square_marcher import _draw_contour
-        grid = np.array([[0, 0, 0, 1],
-                         [0, 1, 1, 1],
-                         [0, 0, 1, 0],
-                         [0, 0, 0, 0]])
-        edges = _draw_contour(grid, 0.5)
-        plt.plot(*edges, color='blue')
+    This is configured for convenience of matplotlib plotting. If you have no intention
+    of post-processing the edges, and just want to visualise the edges using `ax.plot`
+    or similar means, see the below example:
+    ```python
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from square_marcher import _draw_contour
+    grid = np.array([[0, 0, 0, 1],
+                     [0, 1, 1, 1],
+                     [0, 0, 1, 0],
+                     [0, 0, 0, 0]])
+    edges = _draw_contour(grid, 0.5)
+    plt.plot(*edges, color='blue')
+    ```
 
-    ## Parameters:
-    ``grid: numpy.ndarray[int | float]``
-        
-        a `numpy.ndarray` of shape `(rows, cols)`. Each element should be a scalar
-
-    ``threshold: int | float``
     
-        a number representing the threshold to seperate the scalars. The scalars\
-        will be divided into two groups using the threshold: one being greater than the threshold\
-        and one being less than or equal to the threshold.
+    :param numpy.ndarray grid: A `numpy.ndarray` of shape `(rows, cols)`. Each element
+        should be a scalar.
+    :param int or float threshold: A number representing the threshold to seperate the
+        scalars. The scalars will be divided into two groups using the threshold: one
+        being greater than the threshold and one being less than or equal to the threshold.
+    :param bool lerp: Whether to use linear interpolation to determine the coordinates
+        of the edge. If linear interpolation is not used, the coordinates of the edge is
+        at the midpoint between two corners. This defaults to False.
 
-    ``lerp: bool``
-
-        whether to use linear interpolation to determine the coordinates of the edge.\
-            If linear interpolation is not used, the coordinates of the edge is at the\
-            midpoint between two corners. This defaults to False.
-
-    ## Returns
-    A list of tuples of floats. For more information, see above.
+    :return list[tuple[float, float]]: A list of tuples of floats. For more information,
+        see above.
     """
     lines = []
     rows, cols = grid.shape
